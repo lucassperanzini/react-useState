3f81233e0e201530ba469634821a9c2ffc89dc3e
lucassperanzini <rm98297@fiap.com.br>
2025-03-21 11:01:16 -0300

    add Html and click function to activate when the user clicks in the cube

--- src/Componentes/Cube.jsx
+++ src/Componentes/Cube.jsx
@@ -1,11 +1,31 @@
-function Cube({color, position}){
+import { Html } from "@react-three/drei"
+import { useState } from "react"
+import { FaTrashAlt } from "react-icons/fa";
+
+
+function Cube({color, position , id , aoRemover}){
+
+    const [click,setClick] = useState(false)
+
+    function aoClicar(){
+        setClick(!click)
+
+    }
+
 
     return(
-        <mesh position={position}>
-        <boxGeometry/>
-        <meshStandardMaterial color={color}/>
-    </mesh>
+        <>
+
+        
+        {click? <Html center ><div className="bg-green" onClick={()=> aoRemover(id)}><FaTrashAlt size={25} /></div></Html> : null}
+        <mesh onClick={aoClicar} key={id} position={position}>
+            <boxGeometry/>
+            <meshStandardMaterial color={color}/>
+        </mesh>
+        
+        </>
     )
 }
+
 
 export default Cube
